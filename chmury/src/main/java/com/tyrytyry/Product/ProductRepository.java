c84bdb02923a7edcffade375c9688e285f0e447c
Tyrytyry <106188035+Tyrytyry@users.noreply.github.com>
2024-01-20 20:19:58 +0100

    Delete chmury directory

--- chmury/src/main/java/com/tyrytyry/Product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tyrytyry.Product;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<product, Long> {
-}
-
--- chmury/src/main/java/com/tyrytyry/card/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tyrytyry.card;
-
-import com.tyrytyry.card.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
