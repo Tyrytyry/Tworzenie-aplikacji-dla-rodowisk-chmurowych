09b8e75bf2c7184c040057b8b2275b9f39fdab9f
Tyrytyry <106188035+Tyrytyry@users.noreply.github.com>
2024-01-23 21:57:04 +0100

    Delete chmury/chmury/src/main/java/com/tyrytyry directory

--- chmury/chmury/src/main/java/com/tyrytyry/adress/AdresDostawyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tyrytyry.adress;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdresDostawyRepository extends JpaRepository<AdresDostawy, Long> {
-
-}
--- chmury/chmury/src/main/java/com/tyrytyry/data/CardRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.tyrytyry.data;
-//
-//import com.tyrytyry.model.Card;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CardRepository extends JpaRepository<Card, Long> {
-//}
